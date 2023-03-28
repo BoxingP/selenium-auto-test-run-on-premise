be680288b5922f87089f4063b05ec6bcf62b996f
Boxing <boxingp@gmail.com>
2023-03-28 18:47:12 +0800

    CP-20: allow use firefox browser.

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 FROM python:3.8-slim
-RUN apt-get update && apt-get install -y --no-install-recommends cron tzdata lsb-release build-essential wget unzip
+RUN apt-get update && apt-get install -y --no-install-recommends cron tzdata lsb-release build-essential wget unzip libdbus-glib-1-2
 RUN apt-get update && apt-get install -y --no-install-recommends python3-dev python3-virtualenv
 RUN apt-get update && apt-get install -y --no-install-recommends libpq-dev
 WORKDIR /tmp
 RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 RUN apt-get install -y ./google-chrome-stable_current_amd64.deb
-RUN chrome_version="$(google-chrome --version | grep -o -E '[0-9.]+' | cut -d '.' -f1)" \
-    && chromedriver_version=$(wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chrome_version -q -O -) \
-    && wget -q https://chromedriver.storage.googleapis.com/$chromedriver_version/chromedriver_linux64.zip
-RUN unzip -o chromedriver_linux64.zip -d /usr/bin
-RUN chmod 755 /usr/bin/chromedriver
+RUN wget -O firefox-latest-linux64.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64" \
+    && tar xjf ./firefox-latest-linux64.tar.bz2 -C /opt/ \
+    && ln -s /opt/firefox/firefox /usr/bin/firefox
 WORKDIR /usr/src/selenium_auto_test
 ENV VIRTUAL_ENV=/opt/venv
 RUN python3 -m pip install virtualenv
--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class DriverFactory(object):
@@ -7,8 +9,7 @@ class DriverFactory(object):
     def get_driver(browser, headless_mode=False):
         if browser == 'chrome':
             options = webdriver.ChromeOptions()
-            options.add_argument('--window-size=1920,1280')
-            options.add_argument('--enable-javascript')
+            options.add_argument('--start-maximized')
             if headless_mode is True:
                 options.add_argument('--headless')
                 options.add_argument('--disable-infobars')
@@ -18,7 +19,24 @@ class DriverFactory(object):
                 options.add_argument('--single-process')
                 options.add_argument('--ignore-certificate-errors')
                 options.add_argument('--disable-dev-shm-usage')
-            driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
-
+                options.add_argument(
+                    '--user-agent=""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36""')
+            driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
             return driver
+        elif browser == 'firefox':
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--start-maximized')
+            if headless_mode is True:
+                options.add_argument('--headless')
+                options.add_argument('--no-sandbox')
+                options.add_argument('--disable-gpu')
+                options.add_argument('--ignore-certificate-errors')
+                options.add_argument('--allow-running-insecure-content')
+                options.add_argument('--disable-extensions')
+                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument(
+                    '--user-agent=""Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0""')
+            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+            return driver
+
         raise Exception('Provide valid driver name')
