7a1300ce421656ea4b77ce1c69d27edc4acab4eb
Boxing <boxingp@gmail.com>
2023-04-24 18:16:00 +0800

    TC-12: send html email.

--- utils/emails.py
+++ utils/emails.py
@@ -1,6 +1,8 @@
+import os
 import smtplib
 from email import encoders
 from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -13,30 +15,37 @@ class Emails(object):
         self.port = config('SMTP_PORT', cast=int)
         self.sender_email = config('SENDER_EMAIL')
         self.receiver_email = config('RECEIVER_EMAIL', cast=lambda x: x.split(','))
-        self.subject = config('EMAIL_SUBJECT')
-        self.text = config('EMAIL_TEXT').encode().decode('unicode_escape')
+        self.subject = config('EMAIL_SUBJECT').replace('${PROJECT_NAME}', config('PROJECT_NAME'))
+        with open(os.path.join(os.path.dirname(__file__), config('EMAIL_LOGO_FILE')), 'rb') as file:
+            self.logo_img = file.read()
+        with open(os.path.join(os.path.dirname(__file__), config('EMAIL_HTML_FILE')), 'r', encoding='UTF-8') as file:
+            self.html = file.read().replace('${PROJECT_NAME}', config('PROJECT_NAME'))
 
     def send_email(self, tests):
         message = MIMEMultipart()
         message["Subject"] = self.subject
         message["From"] = self.sender_email
         message["To"] = ",".join(self.receiver_email)
-        part = MIMEText(self.text, "plain")
-        message.attach(part)
+        html_part = MIMEText(self.html, "html")
+        message.attach(html_part)
+        logo_image = MIMEImage(self.logo_img)
+        logo_image.add_header('Content-ID', '<logo>')
+        message.attach(logo_image)
+
         content = ''
         index = 1
         for test in tests:
             if test['screenshot'] != '':
                 filename = f"{test['name']}.png"
                 with open(test['screenshot'], "rb") as attachment:
-                    part = MIMEBase("application", "octet-stream")
-                    part.set_payload(attachment.read())
-                encoders.encode_base64(part)
-                part.add_header(
+                    screenshot = MIMEBase("application", "octet-stream")
+                    screenshot.set_payload(attachment.read())
+                encoders.encode_base64(screenshot)
+                screenshot.add_header(
                     "Content-Disposition",
                     f"attachment; filename= {filename}",
                 )
-                message.attach(part)
+                message.attach(screenshot)
 
             sentence = ''
             if len(tests) != 1:
@@ -46,12 +55,12 @@ class Emails(object):
             content = f'{content}\n{sentence}'
             index += 1
 
-        part = MIMEText(f"Failed tests:\n{content}\n", "plain")
-        part.add_header(
+        details_log = MIMEText(f"Failed tests:\n{content}\n", "plain")
+        details_log.add_header(
             "Content-Disposition",
             f"attachment; filename= details.log",
         )
-        message.attach(part)
+        message.attach(details_log)
 
         with smtplib.SMTP(self.smtp_server, self.port) as server:
             server.sendmail(from_addr=self.sender_email, to_addrs=self.receiver_email, msg=message.as_string())
